aacf094764f8735848215c1e624236ea3a1d19fc
JayDragon <809512180@qq.com>
2017-01-05 00:31:01 +0800

    通过CardView来展示水果图片，通过RecyclerView来实现水果列表

---
+++ app/src/main/java/com/gml/materialdemo/Fruit.java
@@ -0,0 +1,24 @@
+package com.gml.materialdemo;
+
+/**
+ * Created by JayDragon on 2017/1/4 0004.
+ */
+
+public class Fruit {
+    private String name;
+    private int imageId;
+
+    public Fruit(String name, int imageId) {
+        this.name = name;
+        this.imageId = imageId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getImageId() {
+        return imageId;
+    }
+
+}
--- app/src/main/java/com/gml/materialdemo/MainActivity.java
+++ app/src/main/java/com/gml/materialdemo/MainActivity.java
@@ -9,14 +9,30 @@ import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Toast;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
 public class MainActivity extends AppCompatActivity {
     private DrawerLayout drawerLayout;
+    private Fruit[] fruits={new Fruit("Apple",R.drawable.apple),new Fruit("Banana",R.drawable.banana)
+    ,new Fruit("Orange",R.drawable.orange),new Fruit("Watermelon",R.drawable.watermelon)
+    ,new Fruit("Pear",R.drawable.pear),new Fruit("Grape",R.drawable.grape)
+    ,new Fruit("Pineapple",R.drawable.pineapple),new Fruit("Strawberry",R.drawable.strawberry)
+    ,new Fruit("Cherry",R.drawable.cherry),new Fruit("Mango",R.drawable.mango)};
+
+    private List<Fruit> fruitList=new ArrayList<>();
+
+    private FruitAdapter adapter;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,10 +47,10 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 Snackbar.make(view,"确定要删除数据？",Snackbar.LENGTH_LONG)
-                        .setAction("取消", new View.OnClickListener() {
+                        .setAction("确定", new View.OnClickListener() {
                             @Override
                             public void onClick(View view) {
-                                Toast.makeText(MainActivity.this, "删除失败！", Toast.LENGTH_SHORT).show();
+                                Toast.makeText(MainActivity.this, "删除成功！", Toast.LENGTH_SHORT).show();
                             }
                         }).show();
 
@@ -53,6 +69,20 @@ public class MainActivity extends AppCompatActivity {
                 return true;
             }
         });
+        initFruits();
+        RecyclerView recyclerView=(RecyclerView)findViewById(R.id.recycler_view);
+        GridLayoutManager layoutManager=new GridLayoutManager(this,2);
+        recyclerView.setLayoutManager(layoutManager);
+        adapter=new FruitAdapter(fruitList);
+        recyclerView.setAdapter(adapter);
+    }
+    private void initFruits(){
+        fruitList.clear();
+        for (int i = 0; i <50 ; i++) {
+            Random random=new Random();
+            int index=random.nextInt(fruits.length);
+            fruitList.add(fruits[index]);
+        }
     }
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
