72cf7a38b3c6e1046ef116c156bd96c21231f778
JayDragon <809512180@qq.com>
2017-01-04 17:24:24 +0800

    添加了悬浮按钮和可交互提示

--- app/src/main/java/com/gml/materialdemo/MainActivity.java
+++ app/src/main/java/com/gml/materialdemo/MainActivity.java
@@ -1,7 +1,9 @@
 package com.gml.materialdemo;
 
 import android.support.annotation.NonNull;
+import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.NavigationView;
+import android.support.design.widget.Snackbar;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBar;
@@ -10,6 +12,7 @@ import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
@@ -23,6 +26,20 @@ public class MainActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
         drawerLayout=(DrawerLayout)findViewById(R.id.drawer_layout);
         NavigationView navigationView=(NavigationView)findViewById(R.id.nav_view);
+        FloatingActionButton fab=(FloatingActionButton)findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view,"确定要删除数据？",Snackbar.LENGTH_LONG)
+                        .setAction("取消", new View.OnClickListener() {
+                            @Override
+                            public void onClick(View view) {
+                                Toast.makeText(MainActivity.this, "删除失败！", Toast.LENGTH_SHORT).show();
+                            }
+                        }).show();
+
+            }
+        });
         ActionBar actionBar=getSupportActionBar();
         if (actionBar!=null){
             actionBar.setDisplayHomeAsUpEnabled(true);
